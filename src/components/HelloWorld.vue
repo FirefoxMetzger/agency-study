fa706cbb5a23ed09edaa38ce8c9cc96ac5187f19
FirefoxMetzger <sebastian@wallkoetter.net>
2018-10-22 09:00:30 +0200

    arranging questionnaire

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <div class="md-layout md-alignment-center-space-around robot-display">
+  <div class="md-layout md-alignment-center-center">
+    <div class="md-layout-item md-layout md-alignment-center-space-around md-size-100 robot-display">
       <Embodiment class="md-layout-item md-size-30" :title="robots.nao.name" :src="robots.nao.image">
         {{descriptions.mentalized}}
       </Embodiment>
@@ -11,16 +11,31 @@
         {{descriptions.mentalized}}
       </Embodiment>
     </div>
-    <LikertScale>
+    <div class="md-layout-item md-size-100">
+      <LikertScale>
       <Question v-for="(question, name, index) in form" :key="index" :class="`index-mod-${index % 2}`"
         v-model="question.response">{{question.text}}</Question>
     </LikertScale>
-    <RankingScale>
-      <RankingQuestion v-for="(question,name,index) in form2" :key="index" 
+    </div>
+    <div class="md-layout-item md-size-95">
+      <div>
+        <p align="left">
+          In this questionnaire you are asked to order above robots based on the criteria desired.
+          The left most item is considered to be highest in the desired capacity, whereas the right most
+          is considered the lowest.
+        </p>
+      </div>
+      <RankingScale>
+      <RankingQuestion v-for="(question,name,index) in form2" :key="index"
         :class="`index-mod-${index % 2}`" v-model="question.response">
-        {{question.text}}  
+        <p v-html="question.text"></p>
       </RankingQuestion>
     </RankingScale>
+    </div>
+    <div class="md-layout-item">
+      <md-button class="md-primary md-raised">Submit</md-button>
+    </div>
+    </div>
   </div>
 </template>
 
@@ -58,16 +73,76 @@ export default {
         mentalized: 'This is a small description.'
       },
       form2: {
-        'q1': {
-          text: 'Please order the NAOs with respect to epicness',
+        'Communication': {
+          text: 'Which character is more capable of <b>conveying thoughts or feelings to others</b>?',
           response: options.slice()
         },
-        'q2': {
-          text: 'Feelings?',
+        'Consciousness': {
+          text: 'Which character is more capable of <b>having experiences and being aware of things</b>?',
           response: options.slice()
         },
-        'q3': {
-          text: 'Badass?',
+        'Desire': {
+          text: 'Which Character is more capable of <b>longing or hoping for things</b>?',
+          response: options.slice()
+        },
+        'Embarrassment': {
+          text: 'Which character is more capable of <b>experiencing embarrassment</b>?',
+          response: options.slice()
+        },
+        'Emotion Recognition': {
+          text: 'Which character is more capable of <b>understanding how others are feeling</b>?',
+          response: options.slice()
+        },
+        'Fear': {
+          text: 'Which character is more capable of <b>feeling afraid or fearful</b>?',
+          response: options.slice()
+        },
+        'Hunger': {
+          text: 'Which character is more capable of <b>capable of feeling hungry</b>?',
+          response: options.slice()
+        },
+        'Joy': {
+          text: 'Which character is more capable of <b>experiencing joy</b>?',
+          response: options.slice()
+        },
+        'Memory': {
+          text: 'Which character is more capable of <b>remembering things</b>?',
+          response: options.slice()
+        },
+        'Morality': {
+          text: 'Which character is more capable of <b>telling right from wrong and trying to do the right thing</b>?',
+          response: options.slice()
+        },
+        'Pain': {
+          text: 'Which character is more capable of <b>experiencing physical or emotional pain</b>?',
+          response: options.slice()
+        },
+        'Personality': {
+          text: 'Which character is more capable of <b>having personality traits that make it unique from others</b>?',
+          response: options.slice()
+        },
+        'Planning': {
+          text: 'Which character is more capable of <b>making plans and working toward a goal</b>?',
+          response: options.slice()
+        },
+        'Pleasure': {
+          text: 'Which character is more capable of <b>experiencing physical or emotional pleasure</b>?',
+          response: options.slice()
+        },
+        'Pride': {
+          text: 'Which character is more capable of <b>experiencing pride</b>?',
+          response: options.slice()
+        },
+        'Rage': {
+          text: 'Which character is more capable of <b>capable of experencing violent or uncontrolled anger</b>?',
+          response: options.slice()
+        },
+        'Self control': {
+          text: 'Which character is more capable of <b>exercising self-restraint over desired, emotions, or impulses</b>?',
+          response: options.slice()
+        },
+        'Thought': {
+          text: 'Which character is more capable of <b>thinking</b>?',
           response: options.slice()
         }
       },
--- src/components/RankingScale.vue
+++ src/components/RankingScale.vue
@@ -2,7 +2,7 @@
   <div class="md-layout">
     <div class="md-layout-item md-layout md-size-100">
       <div class="md-layout-item md-size-25"></div>
-      <div class="md-layout-item" v-for="tick in ticks" :key="tick">{{tick}}</div>
+      <div class="md-layout-item" v-for="tick in ticks" :key="tick"><h3>{{tick}}</h3></div>
     </div>
     <slot></slot>
   </div>
@@ -18,9 +18,9 @@ export default {
     ticks: {
       type: Array,
       default: () => [
-        'low',
-        'medium',
-        'high'
+        'most capabile',
+        '',
+        'least capabile'
       ]
     }
   },
