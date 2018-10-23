c0aa332e7dd0227a5a86a075ca782f0dae468456
FirefoxMetzger <sebastian@wallkoetter.net>
2018-10-23 22:31:18 +0200

    added questionnaires

---
+++ src/components/DescriptionQuestionnaire.vue
@@ -0,0 +1,180 @@
+<template>
+  <div class="md-layout md-alignment-center-center">
+    <div class="md-layout-item md-layout md-alignment-center-space-around md-size-100 robot-display">
+      <Embodiment class="md-layout-item md-size-30" :title="robots.nao.name" :src="robots.nao.image">
+        {{descriptions.mentalized}}
+      </Embodiment>
+      <Embodiment class="md-layout-item md-size-30" :title="robots.nao.name" :src="robots.nao.image">
+        {{descriptions.mentalized}}
+      </Embodiment>
+      <Embodiment class="md-layout-item md-size-30" :title="robots.nao.name" :src="robots.nao.image">
+        {{descriptions.mentalized}}
+      </Embodiment>
+    </div>
+    <div class="md-layout-item md-size-100">
+      <LikertScale>
+      <Question v-for="(question, name, index) in form" :key="index" :class="`index-mod-${index % 2}`"
+        v-model="question.response">{{question.text}}</Question>
+    </LikertScale>
+    </div>
+    <div class="md-layout-item md-size-95">
+      <div>
+        <p align="left">
+          In this questionnaire you are asked to order above robots based on the criteria desired.
+          The left most item is considered to be highest in the desired capacity, whereas the right most
+          is considered the lowest.
+        </p>
+      </div>
+      <RankingScale>
+      <RankingQuestion v-for="(question,name,index) in form2" :key="index"
+        :class="`index-mod-${index % 2}`" v-model="question.response">
+        <p v-html="question.text"></p>
+      </RankingQuestion>
+    </RankingScale>
+    </div>
+    <div class="md-layout-item">
+      <md-button class="md-primary md-raised">Submit</md-button>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import LikertScale from './LikertScale'
+import Question from './Question'
+import Embodiment from './Embodiment'
+import RankingScale from './RankingScale'
+import RankingQuestion from './RankingQuestion'
+
+let options = [
+  'nao',
+  'nao2',
+  'nao3'
+]
+
+export default {
+  components: {
+    LikertScale,
+    Question,
+    Embodiment,
+    RankingScale,
+    RankingQuestion
+  },
+  data () {
+    return {
+      robots: {
+        nao: {
+          image: '/static/NAO.png',
+          icon: '/static/NAO icon.png',
+          name: 'NAO'
+        }
+      },
+      descriptions: {
+        mentalized: 'This is a small description.'
+      },
+      form2: {
+        'Communication': {
+          text: 'Which character is more capable of <b>conveying thoughts or feelings to others</b>?',
+          response: options.slice()
+        },
+        'Consciousness': {
+          text: 'Which character is more capable of <b>having experiences and being aware of things</b>?',
+          response: options.slice()
+        },
+        'Desire': {
+          text: 'Which Character is more capable of <b>longing or hoping for things</b>?',
+          response: options.slice()
+        },
+        'Embarrassment': {
+          text: 'Which character is more capable of <b>experiencing embarrassment</b>?',
+          response: options.slice()
+        },
+        'Emotion Recognition': {
+          text: 'Which character is more capable of <b>understanding how others are feeling</b>?',
+          response: options.slice()
+        },
+        'Fear': {
+          text: 'Which character is more capable of <b>feeling afraid or fearful</b>?',
+          response: options.slice()
+        },
+        'Hunger': {
+          text: 'Which character is more capable of <b>capable of feeling hungry</b>?',
+          response: options.slice()
+        },
+        'Joy': {
+          text: 'Which character is more capable of <b>experiencing joy</b>?',
+          response: options.slice()
+        },
+        'Memory': {
+          text: 'Which character is more capable of <b>remembering things</b>?',
+          response: options.slice()
+        },
+        'Morality': {
+          text: 'Which character is more capable of <b>telling right from wrong and trying to do the right thing</b>?',
+          response: options.slice()
+        },
+        'Pain': {
+          text: 'Which character is more capable of <b>experiencing physical or emotional pain</b>?',
+          response: options.slice()
+        },
+        'Personality': {
+          text: 'Which character is more capable of <b>having personality traits that make it unique from others</b>?',
+          response: options.slice()
+        },
+        'Planning': {
+          text: 'Which character is more capable of <b>making plans and working toward a goal</b>?',
+          response: options.slice()
+        },
+        'Pleasure': {
+          text: 'Which character is more capable of <b>experiencing physical or emotional pleasure</b>?',
+          response: options.slice()
+        },
+        'Pride': {
+          text: 'Which character is more capable of <b>experiencing pride</b>?',
+          response: options.slice()
+        },
+        'Rage': {
+          text: 'Which character is more capable of <b>capable of experencing violent or uncontrolled anger</b>?',
+          response: options.slice()
+        },
+        'Self control': {
+          text: 'Which character is more capable of <b>exercising self-restraint over desired, emotions, or impulses</b>?',
+          response: options.slice()
+        },
+        'Thought': {
+          text: 'Which character is more capable of <b>thinking</b>?',
+          response: options.slice()
+        }
+      },
+      form: {
+        'q1': {
+          text: 'This is a test question that is so long that it will cause wierd formatting',
+          response: null
+        },
+        'q2': {
+          text: 'This is a test question that is so long that it will cause wierd formatting',
+          response: null
+        },
+        'q3': {
+          text: 'This is a test question that is so long that it will cause wierd formatting',
+          response: null
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.robot-display{
+  margin-bottom: 40px;
+}
+
+.index-mod-0{
+
+}
+
+.index-mod-1{
+  background-color: lightgray;
+}
+</style>
---
+++ src/components/PreQuestionnaire.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="md-layout md-alignment-center-center">
+    <div class="md-layout-item md-size-90 md-elevation-1">
+      <p>
+        Before we begin, please answer some general questions about you.
+        (This will be TIPI and NARS)
+      </p>
+    </div>
+    <div class="md-layout-item md-size-95 md-elevation-1">
+      <p align="left">This is NARS</p>
+      <LikertScale>
+        <Question v-for="(question, name, index) in form" :key="index" :class="`index-mod-${index % 2}`"
+          v-model="question.response">{{question.text}}</Question>
+      </LikertScale>
+    </div>
+    <div class="md-layout-item md-size-95 md-elevation-1">
+      <p align="left">This is TIPI</p>
+      <LikertScale points=7 :ticks="tipi_ticks">
+        <Question v-for="(question, name, index) in form" :key="index" :class="`index-mod-${index % 2}`"
+          v-model="question.response" points=7>{{question.text}}</Question>
+      </LikertScale>
+    </div>
+    <div class="md-layout-item">
+      <md-button class="md-primary md-raised" v-on:click="submit">Submit</md-button>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import LikertScale from './LikertScale'
+import Question from './Question'
+
+export default {
+  components: {
+    LikertScale,
+    Question
+  },
+  data () {
+    return {
+      form: {
+        'q1': {
+          text: 'This is a test question that is so long that it will cause wierd formatting',
+          response: null
+        },
+        'q2': {
+          text: 'This is a test question that is so long that it will cause wierd formatting',
+          response: null
+        },
+        'q3': {
+          text: 'This is a test question that is so long that it will cause wierd formatting',
+          response: null
+        }
+      },
+      form2: {
+        'Extraversion': {
+          text: 'Extraverted, enthusiastic',
+          response: null
+        },
+        'Agreeableness Reversed': {
+          text: 'Critical, quarrelsome',
+          response: null
+        },
+        'Conscientiousness': {
+          text: 'Dependable, self-disciplined',
+          response: null
+        },
+        'Emotional Stability Reversed': {
+          text: 'Anxious, easily upset.',
+          response: null
+        },
+        'Openness to Experiences': {
+          text: 'Open to new experiences, complex.',
+          response: null
+        },
+        'Extraversion Reversed': {
+          text: 'Reserved, quiet.',
+          response: null
+        },
+        'Agreeableness': {
+          text: 'Sympathetic, warm.',
+          response: null
+        },
+        'Conscientiousness Reversed': {
+          text: 'Disorganized, careless.',
+          response: null
+        },
+        'Emotional Stability': {
+          text: 'Calm, emotionally stable.',
+          response: null
+        },
+        'Openness to Experiences Reversed': {
+          text: 'Conventional, uncreative.',
+          response: null
+        }
+      },
+      tipi_ticks: [
+        '',
+        'Disagree strongly',
+        'Disagree moderately',
+        'Disagree a little',
+        'Neither agree nor disagree',
+        'Agree a little',
+        'Agree moderately',
+        'Agree strongly'
+      ]
+    }
+  },
+  methods: {
+    submit: function() {
+      this.$router.push({path:"embodiment"})
+    }
+  }
+}
+</script>
+
+<style scoped>
+.robot-display{
+  margin-bottom: 40px;
+}
+
+.index-mod-0{
+
+}
+
+.index-mod-1{
+  background-color: lightgray;
+}
+
+.md-layout-item{
+  margin-top: 5px;
+  margin-bottom: 5px;
+}
+</style>
